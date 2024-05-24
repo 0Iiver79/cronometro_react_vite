5fc6f6f661de7334944fba1e561141d3d063b01a
Gustavo Oliveira Cruz <gustavovizisbr@gmail.com>
2024-05-23 22:52:09 -0300

    configuracoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/cronometro_react-vite",
+  base:"/cronometro_react_vite",
 })
